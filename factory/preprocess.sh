23f795f04f33424653e737d2fc62572f7ceabb61
Roland Chastain <roland.chastain@gmx.fr>
2026-06-20 15:08:54 +0200

    Move files

--- factory/preprocess.sh
+++ factory/preprocess.sh
@@ -2,4 +2,4 @@ PREPROC=/home/roland/apps/pasdoc/1_0_4/bin/pascal_pre_proc
 
 # https://pasdoc.github.io/OtherTools#_-pascal_pre_proc
 
-$PREPROC constants1.pas > constants.pas
+$PREPROC constants.pas > ../constants.pas
